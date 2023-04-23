158c6e946a84780f561428b66a5da36cf33d1f74
AbexCorp <bartek.fryczek@onet.pl>
2023-04-23 22:53:14 +0200

    Create BitMatrix_ICloneable.cs

---
+++ BitMatrixProject/BitMatrixProject/BitMatrixL/BitMatrix_ICloneable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+public partial class BitMatrix : ICloneable
+{
+    public object Clone()
+    {
+        BitMatrix clonedMatrix = new BitMatrix(NumberOfRows, NumberOfColumns);
+        for(int i = 0; i < clonedMatrix.data.Length; i++)
+        {
+            clonedMatrix.data[i] = data[i];
+        }
+        return clonedMatrix;
+    }
+}
