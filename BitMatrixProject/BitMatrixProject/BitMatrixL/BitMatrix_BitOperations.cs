caaeac365a47687270c906d5e8a880e55970f2a3
AbexCorp <bartek.fryczek@onet.pl>
2023-04-26 20:49:21 +0200

    Bit Operations

---
+++ BitMatrixProject/BitMatrixProject/BitMatrixL/BitMatrix_BitOperations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+public partial class BitMatrix
+{
+    private void CheckIfSameSizes(BitMatrix other)
+    {
+        if (ReferenceEquals(other, null))
+            throw new ArgumentNullException();
+        if (NumberOfRows != other.NumberOfRows || NumberOfColumns != other.NumberOfColumns)
+            throw new ArgumentException();
+    }
+    public BitMatrix And(BitMatrix other)
+    {
+        CheckIfSameSizes(other);
+        data = data.And(other.data);
+        return this;
+    }
+
+    public BitMatrix Or(BitMatrix other)
+    {
+        CheckIfSameSizes(other);
+        data = data.Or(other.data);
+        return this;
+    }
+    public BitMatrix Xor(BitMatrix other)
+    {
+        CheckIfSameSizes(other);
+        data = data.Xor(other.data);
+        return this;
+    }
+    public BitMatrix Not()
+    {
+        data = data.Not();
+        return this;
+    }
+
+
+
+    private static void CheckIfSameSizes(BitMatrix matrix1, BitMatrix matrix2)
+    {
+        if (ReferenceEquals(matrix1, null) || ReferenceEquals(matrix2, null))
+            throw new ArgumentNullException();
+        if (matrix1.NumberOfRows != matrix2.NumberOfRows || matrix1.NumberOfColumns != matrix2.NumberOfColumns)
+            throw new ArgumentException();
+    }
+    private static void CheckIfNull(BitMatrix matrix)
+    {
+        if (ReferenceEquals(matrix, null))
+            throw new ArgumentNullException();
+    }
+
+    public static BitMatrix operator &(BitMatrix matrix1, BitMatrix matrix2)
+    {
+        CheckIfSameSizes(matrix1, matrix2);
+        BitMatrix temp = (BitMatrix)matrix1.Clone();
+        temp = temp.And((BitMatrix)matrix2.Clone());
+        return temp;
+    }
+    public static BitMatrix operator |(BitMatrix matrix1, BitMatrix matrix2)
+    {
+        CheckIfSameSizes(matrix1, matrix2);
+        BitMatrix temp = (BitMatrix)matrix1.Clone();
+        temp = temp.Or((BitMatrix)matrix2.Clone());
+        return temp;
+    }
+    public static BitMatrix operator ^(BitMatrix matrix1, BitMatrix matrix2)
+    {
+        CheckIfSameSizes(matrix1, matrix2);
+        BitMatrix temp = (BitMatrix)matrix1.Clone();
+        temp = temp.Xor((BitMatrix)matrix2.Clone());
+        return temp;
+    }
+    public static BitMatrix operator !(BitMatrix matrix)
+    {
+        CheckIfNull(matrix);
+        BitMatrix temp = (BitMatrix)matrix.Clone();
+        temp = temp.Not();
+        return temp;
+    }
+}
