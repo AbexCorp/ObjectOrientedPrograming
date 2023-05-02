7326a2dca99de1588a6a08a94d6530e6dbc21a20
AbexCorp <bartek.fryczek@onet.pl>
2023-05-02 02:23:09 +0200

    Time ToString Equatable Comparable Operators

--- Time-TimePeriod/Time-TimePeriod/Program.cs
+++ Time-TimePeriod/Time-TimePeriod/Program.cs
@@ -12,7 +12,101 @@ namespace TimeTimePeriod
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine();
+            Console.WriteLine("Constructors");
+            Console.WriteLine("--------------------------");
+
+            Time t = new Time();
+            Console.WriteLine(t);
+
+            t = new Time();
+            Console.WriteLine(t);
+
+            t = new Time(15,29,10);
+            Console.WriteLine(t);
+
+            t = new Time(21,37);
+            Console.WriteLine(t);
+
+            t = new Time(9);
+            Console.WriteLine(t);
+
+            t = new Time(DateTime.Now);
+            Console.WriteLine(t);
+
+            t = new Time(DateTimeOffset.Now);
+            Console.WriteLine(t);
+
+            t = new Time("22:15:49");
+            Console.WriteLine(t);
+
+            t = new Time("4:20");
+            Console.WriteLine(t);
+
+            Console.WriteLine();
+            Console.WriteLine("Operators");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("== !=");
+            Time t1 = new Time("17:45:15");
+            Time t2 = new Time("17:45:15");
+            Console.WriteLine(t1 == t2);
+            Console.WriteLine(!(t1 != t2));
+            t2 = new Time("17:45:6");
+            Console.WriteLine(!(t1 == t2));
+            Console.WriteLine(t1 != t2);
+            t2 = new Time("17:10:15");
+            Console.WriteLine(!(t1 == t2));
+            Console.WriteLine(t1 != t2);
+            t2 = new Time("10:45:15");
+            Console.WriteLine(!(t1 == t2));
+            Console.WriteLine(t1 != t2);
+
+            Console.WriteLine("< >");
+            t1 = new Time("14:30:20");
+            t2 = new Time("14:30:20");
+            Console.WriteLine(!(t1 > t2));
+            Console.WriteLine(!(t2 > t1));
+            Console.WriteLine(!(t1 < t2));
+            Console.WriteLine(!(t2 < t1));
+            t2 = new Time("14:30:10");
+            Console.WriteLine(t1 > t2);
+            Console.WriteLine(!(t2 > t1));
+            Console.WriteLine(!(t1 < t2));
+            Console.WriteLine(t2 < t1);
+            t2 = new Time("14:10:20");
+            Console.WriteLine(t1 > t2);
+            Console.WriteLine(!(t2 > t1));
+            Console.WriteLine(!(t1 < t2));
+            Console.WriteLine(t2 < t1);
+            t2 = new Time("10:30:20");
+            Console.WriteLine(t1 > t2);
+            Console.WriteLine(!(t2 > t1));
+            Console.WriteLine(!(t1 < t2));
+            Console.WriteLine(t2 < t1);
+
+            Console.WriteLine("<= >=");
+            t1 = new Time("14:30:20");
+            t2 = new Time("14:30:20");
+            Console.WriteLine((t1 >= t2));
+            Console.WriteLine((t2 >= t1));
+            Console.WriteLine((t1 <= t2));
+            Console.WriteLine((t2 <= t1));
+            t2 = new Time("14:30:10");
+            Console.WriteLine(t1 >= t2);
+            Console.WriteLine(!(t2 >= t1));
+            Console.WriteLine(!(t1 <= t2));
+            Console.WriteLine(t2 <= t1);
+            t2 = new Time("14:10:20");
+            Console.WriteLine(t1 >= t2);
+            Console.WriteLine(!(t2 >= t1));
+            Console.WriteLine(!(t1 <= t2));
+            Console.WriteLine(t2 <= t1);
+            t2 = new Time("10:30:20");
+            Console.WriteLine(t1 >= t2);
+            Console.WriteLine(!(t2 >= t1));
+            Console.WriteLine(!(t1 <= t2));
+            Console.WriteLine(t2 <= t1);
         }
     }
 }
--- Time-TimePeriod/Time-TimePeriod/Structures/Time.cs
+++ Time-TimePeriod/Time-TimePeriod/Structures/Time.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TimeTimePeriod.Structures
 {
-    struct Time
+    struct Time : IEquatable<Time>, IComparable<Time>
     {
         // <<< Variables >>>
         private readonly byte _hours;
@@ -52,24 +53,27 @@ namespace TimeTimePeriod.Structures
                 throw new ArgumentNullException();
 
             string[] splitText = text.Split(':', StringSplitOptions.RemoveEmptyEntries);
-            if (splitText.Length != 3)
-                throw new ArgumentException();
+            if (splitText.Length > 3 || splitText.Length < 2)
+                throw new FormatException();
 
             _hours = ValidateHours(int.Parse(splitText[0]));
-            _minutes = ValidateHours(int.Parse(splitText[1]));
-            _seconds = ValidateHours(int.Parse(splitText[2]));
+            _minutes = ValidateMinutesOrSeconds(int.Parse(splitText[1]));
+            if(splitText.Length == 3)
+                _seconds = ValidateMinutesOrSeconds(int.Parse(splitText[2]));
+            else
+                _seconds = 0;
         }
         public Time(DateTime dateTime)
         {
             _hours = (byte)dateTime.Hour;
             _minutes = (byte)dateTime.Minute;
-            _seconds = (byte)dateTime.Millisecond;
+            _seconds = (byte)dateTime.Second;
         }
         public Time(DateTimeOffset dateTimeOffset)
         {
             _hours = (byte)dateTimeOffset.Hour;
             _minutes = (byte)dateTimeOffset.Minute;
-            _seconds = (byte)dateTimeOffset.Millisecond;
+            _seconds = (byte)dateTimeOffset.Second;
         }
 
         private static byte ValidateHours(int hours)
@@ -83,6 +87,81 @@ namespace TimeTimePeriod.Structures
             if (minutesOrSeconds > 59 || minutesOrSeconds < 0)
                 throw new ArgumentOutOfRangeException();
             return (byte)minutesOrSeconds;
+        }
+
+
+
+        // <<< ToString >>>
+        public override string ToString()
+        {
+            return ($"{_hours.ToString("D2")}:{_minutes.ToString("D2")}:{_seconds.ToString("D2")}");
+        }
+
+
+
+        // <<< Equatable >>>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            if(obj is Time)
+                return Equals((Time)obj);
+            return false;
+        }
+        public bool Equals(Time other)
+        {
+            if( (_hours != other.Hours) || (_minutes != other.Minutes) || (_seconds != other.Seconds) )
+                return false;
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine( _hours, _minutes, _seconds );
+        }
+
+
+
+        // <<< Comparable >>>
+        public int CompareTo(Time other)
+        {
+            int result = _hours.CompareTo(other.Hours);
+            if( result != 0 )
+                return result;
+
+            result = _minutes.CompareTo(other.Minutes);
+            if (result != 0)
+                return result;
+
+            result = _seconds.CompareTo(other.Seconds);
+            return result;
+        }
+
+
+
+        // <<< Operators >>>
+        public static bool operator ==(Time t1, Time t2)
+        {
+            return t1.Equals(t2);
+        }
+        public static bool operator !=(Time t1, Time t2)
+        {
+            return !t1.Equals(t2);
+        }
+        public static bool operator >(Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) > 0 ? true : false;
+        }
+        public static bool operator <(Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) < 0 ? true : false;
+        }
+        public static bool operator >=(Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) >= 0 ? true : false;
+        }
+        public static bool operator <=(Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) <= 0 ? true : false;
         }
 
     }
