315b591ee619bdeebd9b39309d0772119a4cd13e
AbexCorp <bartek.fryczek@onet.pl>
2023-05-26 22:19:15 +0200

    Copier zad3 start

---
+++ CopierProject/Zadanie3/Printer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ver1;
+
+namespace Zadanie3
+{
+    public class Printer : BaseDevice, IPrinter
+    {
+        #region <<< Variables >>>
+
+        private int _counter = 0;
+        private int _printCounter = 0;
+
+        #endregion
+
+
+        #region <<< Properties >>>
+
+        new public int Counter { get { return _counter; } } //zwraca liczbę uruchomień kserokopiarki
+        public int PrintCounter { get { return _printCounter; } } //zwraca aktualną liczbę wydrukowanych dokumentów,
+
+        private bool IsTurnedOn
+        {
+            get
+            {
+                switch (this.GetState())
+                {
+                    case IDevice.State.on:
+                        return true;
+                    case IDevice.State.off:
+                    default:
+                        return false;
+                }
+            }
+        }
+        private bool IsTurnedOff => !IsTurnedOn;
+
+        #endregion
+
+
+        #region <<< BaseDevice >>>
+
+        public new void PowerOn()
+        {
+            if (IsTurnedOn) 
+                return;
+            _counter++;
+            state = IDevice.State.on;
+        }
+
+        public new void PowerOff()
+        {
+            if (IsTurnedOff) 
+                return;
+            state = IDevice.State.off;
+        }
+
+        #endregion
+
+        public void Print(in IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CopierProject/Zadanie3/Scanner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ver1;
+
+namespace Zadanie3
+{
+    public class Scanner : BaseDevice, IScanner
+    {
+        #region <<< Variables >>>
+
+        private int _counter = 0;
+        private int _scanCounter = 0;
+
+        #endregion
+
+
+        #region <<< Properties >>>
+
+        new public int Counter { get { return _counter; } } //zwraca liczbę uruchomień kserokopiarki
+        public int ScanCounter { get {  return _scanCounter; } } //zwraca liczbę zeskanowanych dokumentów,
+
+        private bool IsTurnedOn 
+        {
+            get
+            {
+                switch (this.GetState())
+                {
+                    case IDevice.State.on:
+                        return true;
+                    case IDevice.State.off:
+                    default:
+                        return false;
+                }
+            }
+        }
+        private bool IsTurnedOff => !IsTurnedOn;
+
+        #endregion
+
+
+        #region <<< BaseDevice >>>
+
+        public new void PowerOn()
+        {
+            if (IsTurnedOn) 
+                return;
+            _counter++;
+            state = IDevice.State.on;
+        }
+
+        public new void PowerOff()
+        {
+            if (IsTurnedOff) 
+                return;
+            state = IDevice.State.off;
+        }
+
+        #endregion
+
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
