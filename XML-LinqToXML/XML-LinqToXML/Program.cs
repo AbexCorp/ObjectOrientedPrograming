c26d6816a18a6dc4a22c4fae68062b27db909e53
AbexCorp <bartek.fryczek@onet.pl>
2023-08-16 00:06:52 +0200

    XmlLinxXml some data collection

--- XML-LinqToXML/XML-LinqToXML/Program.cs
+++ XML-LinqToXML/XML-LinqToXML/Program.cs
@@ -14,21 +14,8 @@ namespace XMLToLinqToXMLConverter
                 throw new Exception("Something went wrong when copying the file");
 
 
-            string volume = (issues
-                ?.Descendants("issue")
-                ?.Descendants("volume")
-                ?.FirstOrDefault() ?? throw new ArgumentException("Volume not found"))
-                .Value;
-            string number = (issues
-                ?.Descendants("issue")
-                ?.Descendants("number")
-                ?.FirstOrDefault() ?? throw new ArgumentException("Number not found"))
-                .Value;
-            string year = (issues
-                ?.Descendants("issue")
-                ?.Descendants("year")
-                ?.FirstOrDefault() ?? throw new ArgumentException("Year not found"))
-                .Value;
+            GetIssueMetaData(in issues, out string year, out string number, out string volume);
+
 
             var articles = issues
                 ?.Descendants("issue")
@@ -41,17 +28,25 @@ namespace XMLToLinqToXMLConverter
             XDocument newFile = new();
             foreach(XElement article in articles)
             {
-                fullDoi = (article
-                    ?.Descendants("galley")
-                    ?.Descendants("file")
-                    ?.Descendants("remote")
-                    ?.Attributes("src")
-                    ?.FirstOrDefault() ?? throw new ArgumentException("Doi source was not found"))
-                    .Value;
+                //Collect data
+                fullDoi = GetArticleDoi(in article).Trim();
                 string[] doi = fullDoi.Split('-', StringSplitOptions.RemoveEmptyEntries);
                 doi = new string[] { doi[doi.Length - 2], doi[doi.Length-1] };
-                Console.WriteLine($"{doi[0]},{doi[1]}");
 
+                string enUsTitle = GetArticleTitle(in article);
+                string enUsText = GetArticleText(in article);
+                string[] enUsKeywords = GetArticleKeywords(in article);
+                //authors
+                string pages = GetArticlePages(in article);
+                string publishDate = GetArticlePublishDate(in article);
+                //permissions
+                //galley
+
+
+
+
+
+                //Convert data
                 //newFile = new XDocument(
                 //    new XDeclaration("1.0", "utf-8", "yes"),
                 //    new XElement("Article")
@@ -62,6 +57,90 @@ namespace XMLToLinqToXMLConverter
             //string newfile = Path.Combine(Directory.GetCurrentDirectory(), "issues.xml");
             //File.CreateText(newfile);
             //File.AppendAllLines(newfile, issue.ToString().);
+        }
+
+
+
+        private static bool GetIssueMetaData(in XDocument issues, out string year, out string number, out string volume)
+        {
+            try
+            {
+                volume = (issues
+                    ?.Descendants("issue")
+                    ?.Descendants("volume")
+                    ?.FirstOrDefault() ?? throw new ArgumentException("Volume not found"))
+                    .Value;
+                number = (issues
+                    ?.Descendants("issue")
+                    ?.Descendants("number")
+                    ?.FirstOrDefault() ?? throw new ArgumentException("Number not found"))
+                    .Value;
+                year = (issues
+                    ?.Descendants("issue")
+                    ?.Descendants("year")
+                    ?.FirstOrDefault() ?? throw new ArgumentException("Year not found"))
+                    .Value;
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                volume = null;
+                number = null;
+                year = null;
+                return false;
+            }
+        }
+        private static string GetArticleDoi(in XElement article)
+        {
+            string fullDoi = (article
+                ?.Descendants("galley")
+                ?.Descendants("file")
+                ?.Descendants("remote")
+                ?.Attributes("src")
+                ?.FirstOrDefault() ?? throw new ArgumentException("Doi source was not found"))
+                .Value;
+            return fullDoi;
+        }
+        private static string GetArticleTitle(in XElement article)
+        {
+            string enUsTitle = (article
+                ?.Descendants("title")
+                ?.FirstOrDefault(x => (x.Attribute("locale") ?? throw new ArgumentException("Locale not found")).Value == "en_US") ?? throw new ArgumentException("Title not found"))
+                .Value;
+            return enUsTitle;
+        }
+        private static string GetArticleText(in XElement article)
+        {
+            string enUsText = (article
+                ?.Descendants("abstract")
+                ?.FirstOrDefault(x => (x.Attribute("locale") ?? throw new ArgumentException("Locale not found")).Value == "en_US") ?? throw new ArgumentException("Article text not found"))
+                .Value;
+            return enUsText;
+        }
+        private static string[] GetArticleKeywords(in XElement article)
+        {
+            string enUsKeywords = (article
+                ?.Descendants("indexing")
+                ?.Descendants("subject")
+                ?.FirstOrDefault(x => (x.Attribute("locale") ?? throw new ArgumentException("Locale not found")).Value == "en_US") ?? throw new ArgumentException("Keywords not found"))
+                .Value;
+            return enUsKeywords.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        }
+        private static string GetArticlePages(in XElement article)
+        {
+            string pages = (article
+                ?.Descendants("pages")
+                ?.FirstOrDefault() ?? throw new ArgumentException("Pages not found"))
+                .Value;
+            return pages;
+        }
+        private static string GetArticlePublishDate(in XElement article)
+        {
+            string publishDate = (article
+                ?.Descendants("date_published")
+                ?.FirstOrDefault() ?? throw new ArgumentException("Publish date not found"))
+                .Value;
+            return publishDate;
         }
     }
 }
